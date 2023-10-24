1d866192c610aec8edb3029f614714d108964970
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-10-24 13:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+   this.age=age;
 
-function Employee(name, age, jobTitle) {}
+   Person.prototype.greet = function ()
+   {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+   }
+}
+
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age); // if we directlycall person() , without any object it will direct to window object also we cant use this.person() as it permanently attached function.
+
+   this.jobTitle= jobTitle;
+
+   Employee.prototype.__proto__=Person.prototype;
+
+   Employee.prototype.jobGreet = function ()
+   {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+   }
+}
 
 // Do not change code below this line
 window.Person = Person;
